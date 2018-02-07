869c01e76a2b06724b20519cb67094b2441ccee7
popcornXL <st060917@gmail.com>
2018-02-07 16:47:28 +0800

    project second commit

---
+++ src/main/java/com/mmall/controller/portal/UserController.java
@@ -0,0 +1,133 @@
+package com.mmall.controller.portal;
+
+import com.mmall.common.Const;
+import com.mmall.common.ResponseCode;
+import com.mmall.common.ServerResponse;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import net.sf.jsqlparser.schema.Server;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpSession;
+
+@Controller
+@RequestMapping("/user/")
+public class UserController {
+
+    @Autowired
+    private IUserService iUserService;
+    /**
+     * 用戶登入
+     * @param username
+     * @param password
+     * @param session
+     * @return
+     */
+    @RequestMapping(value = "login.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> login(String username, String password, HttpSession session){
+        ServerResponse<User> response = iUserService.login(username,password);
+        if(response.isSuccess()){
+
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+        }
+            return response;
+    }
+
+    //用戶登出
+    @RequestMapping(value = "logout.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> logout(HttpSession session){
+        session.removeAttribute(Const.CURRENT_USER);
+        return  ServerResponse.creatBySuccess();
+    }
+
+    //用戶註冊
+    @RequestMapping(value = "register.do", method = RequestMethod.POST)
+    @ResponseBody
+    public  ServerResponse<String> register(User user){
+        return iUserService.register(user);
+    }
+
+    //校驗功能
+    @RequestMapping(value = "check_valid.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String>  checkValid(String str, String type){
+        return  iUserService.checkValid(str,type);
+    }
+    //獲得登入用戶資料
+    @RequestMapping(value = "get_user_info.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> getUserInfo(HttpSession session){
+        User user = (User) session.getAttribute(Const.CURRENT_USER);
+        if(user != null)
+        {
+            return  ServerResponse.creatBySuccess(user);
+        }
+        return  ServerResponse.createByErrorMessage("用戶未登入無法獲得當前用戶資料");
+    }
+
+    //忘記密碼
+    @RequestMapping(value = "forget_get_question.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetGetQuestion(String username){
+        return  iUserService.selectQuestion(username);
+    }
+    //使用本地緩存 檢查問題答案
+    @RequestMapping(value = "forget_check_answer.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetCheckAnswer(String username,String question,String answer){
+        return  iUserService.checkAnswer(username,question,answer);
+    }
+    //未登入重置密碼
+    @RequestMapping(value = "forget_reset_password.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> forgetResetPassword(String username,String passwordNew,String forgetToken){
+        return  iUserService.forgetResetPassword(username,passwordNew,forgetToken);
+    }
+    //登入重置密碼
+    @RequestMapping(value = "reset_password.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<String> resetPassword(HttpSession session,String passwordOld,String passwordNew){
+        User user = (User)session.getAttribute(Const.CURRENT_USER);
+        if(user ==null){
+            return ServerResponse.createByErrorMessage("用戶未登錄");
+        }
+        return iUserService.resetPassword(passwordOld,passwordNew,user);
+    }
+
+    //更新個人資料
+    @RequestMapping(value = "update_information.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> update_information(HttpSession session,User user){
+        User current_User = (User)session.getAttribute(Const.CURRENT_USER);
+        if(current_User ==null){
+            return ServerResponse.createByErrorMessage("用戶未登錄");
+        }
+        user.setId(current_User.getId());//從session取id
+        user.setUsername(current_User.getUsername());//從session取用戶名
+        ServerResponse<User> response = iUserService.updateInformation(user);
+        if(response.isSuccess()){
+            response.getData().setUsername(current_User.getUsername());
+            session.setAttribute(Const.CURRENT_USER,response.getData());
+        }
+        return response;
+    }
+
+    //獲得用戶資訊
+    @RequestMapping(value = "get_information.do", method = RequestMethod.POST)
+    @ResponseBody
+    public ServerResponse<User> get_information(HttpSession session){
+        User currentUser = (User)session.getAttribute(Const.CURRENT_USER);
+        if(currentUser ==null){
+            return ServerResponse.createByErrorCodeMessage(ResponseCode.NEED_LOGIN.getCode(),"未登錄，需要強制登錄status=10");
+        }
+        return  iUserService.getInformation(currentUser.getId());
+    }
+
+}
---
+++ src/main/java/com/mmall/service/impl/UserServiceImpl.java
@@ -0,0 +1,174 @@
+package com.mmall.service.impl;
+
+import com.mmall.common.Const;
+import com.mmall.common.ServerResponse;
+import com.mmall.common.TokenCache;
+import com.mmall.dao.UserMapper;
+import com.mmall.pojo.User;
+import com.mmall.service.IUserService;
+import com.mmall.util.MD5Util;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service("iUserService")
+public class UserServiceImpl implements IUserService{
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @Override
+    public ServerResponse<User> login(String username, String password) {
+        int resultCount = userMapper.checkUsername(username);
+        if(resultCount == 0){
+            return ServerResponse.createByErrorMessage("用戶名不存在");
+        }
+
+        String md5Password = MD5Util.MD5EncodeUtf8(password);
+        User user=userMapper.selectLogin(username,md5Password);
+        if(user == null){
+            return ServerResponse.createByErrorMessage("密碼錯誤");
+        }
+
+        user.setPassword(StringUtils.EMPTY);
+        return  ServerResponse.creatBySuccess("登入成功",user);
+    }
+
+    public ServerResponse<String> register(User user) {
+        ServerResponse validResponse = this.checkValid(user.getUsername(),Const.USERNAME);
+        if(!validResponse.isSuccess()){
+            return  validResponse;
+        }
+         validResponse = this.checkValid(user.getEmail(),Const.EMAIL);
+        if(!validResponse.isSuccess()){
+            return  validResponse;
+        }
+        user.setRole(Const.Role.ROLE_CUSTOMER);
+        //MD5加密
+        user.setPassword(MD5Util.MD5EncodeUtf8(user.getPassword()));
+        int resultCount = userMapper.insert(user);
+        if (resultCount == 0) {
+            return ServerResponse.createByErrorMessage("註冊失敗");
+        }
+        return ServerResponse.creatBySuccessMessage("註冊成功");
+    }
+
+    public ServerResponse<String> checkValid(String str,String type){
+        if(StringUtils.isNotBlank(type)){
+            //開始校驗
+            if(Const.USERNAME.equals(type)){
+                int resultCount = userMapper.checkUsername(str);
+                if (resultCount > 0) {
+                    return ServerResponse.createByErrorMessage("用戶名已存在");
+                }
+            }
+            if(Const.EMAIL.equals(type)){
+                int resultCount = userMapper.checkEmail(str);
+                if (resultCount > 0) {
+                    return ServerResponse.createByErrorMessage("email已存在");
+                }
+            }
+        }else{
+            return  ServerResponse.createByErrorMessage("參數錯誤");
+        }
+        return  ServerResponse.creatBySuccessMessage("校驗成功");
+    }
+
+    public  ServerResponse selectQuestion(String username){
+        ServerResponse validResponse = this.checkValid(username,Const.USERNAME);
+        if(validResponse.isSuccess()){
+            //用戶不存在
+            return  ServerResponse.createByErrorMessage("用戶不存在");
+        }
+        String question = userMapper.selectQuestionByUsername(username);
+        if(StringUtils.isNotBlank(question)){
+            return  ServerResponse.creatBySuccess(question);
+        }
+        return ServerResponse.createByErrorMessage("找回密碼的問題是空的");
+    }
+
+
+    public ServerResponse<String> checkAnswer(String username,String question,String answer){
+        int resultCount = userMapper.checkAnswer(username,question,answer);
+        if(resultCount > 0){
+            //說明問題與答案是此用戶的，且正確。
+            String forgetToken = UUID.randomUUID().toString();
+            TokenCache.setKey(TokenCache.TOKEN_PREFIX+username,forgetToken);
+            return  ServerResponse.creatBySuccess(forgetToken);
+        }
+        return ServerResponse.createByErrorMessage("問題的答案錯誤");
+    }
+
+    public  ServerResponse<String> forgetResetPassword(String username,String passwordNew,String forgetToken) {
+        if (StringUtils.isBlank(forgetToken)) {
+            return ServerResponse.createByErrorMessage("參數錯誤,token需要傳遞");
+        }
+        ServerResponse validResponse = this.checkValid(username, Const.USERNAME);
+        if (validResponse.isSuccess()) {
+            //用戶不存在
+            return ServerResponse.createByErrorMessage("用戶不存在");
+        }
+        String token = TokenCache.getKey(TokenCache.TOKEN_PREFIX+username);
+        if(StringUtils.isBlank(token)){
+            return  ServerResponse.createByErrorMessage("token過期或無效");
+        }
+        if(StringUtils.equals(forgetToken,token)){
+            String md5Password = MD5Util.MD5EncodeUtf8(passwordNew);
+            int rowCount = userMapper.updatePasswordByUsername(username,md5Password);
+            if(rowCount > 0){
+                return ServerResponse.creatBySuccessMessage("修改密碼成功");
+            }
+        }
+        else{
+            return  ServerResponse.createByErrorMessage("token錯誤,需要重取token");
+        }
+        return  ServerResponse.createByErrorMessage("修改密碼失敗");
+    }
+
+    public ServerResponse<String> resetPassword(String passwordOld,String passwordNew,User user){
+        //防止橫向越權，因此需要校驗用戶舊密碼，一定要指定是這個用戶，因為我們會查詢一個count(1),如果不指定id，那結果就是true
+        int resultCount = userMapper.checkPassword(MD5Util.MD5EncodeUtf8(passwordOld),user.getId());
+        if(resultCount  == 0){
+            return  ServerResponse.createByErrorMessage("舊密碼錯誤");
+        }
+        user.setPassword(MD5Util.MD5EncodeUtf8(passwordNew));
+        int updateCount = userMapper.updateByPrimaryKeySelective(user);
+        if(updateCount > 0){
+            return  ServerResponse.creatBySuccessMessage("密碼更新成功");
+        }
+        return  ServerResponse.createByErrorMessage("密碼更新失敗");
+    }
+
+    public ServerResponse<User> updateInformation(User user) {
+        //username是不能被更新的
+        //email也要進行一個校驗，校驗新的email是否已存在，若與存在的email相同的話，不能被更新。
+        int resultCount = userMapper.checkEmailByUserId(user.getEmail(), user.getId());
+        if (resultCount > 0) {
+            return ServerResponse.creatBySuccessMessage("EMAIL重複，請更換email再嘗試更新");
+        }
+        User updateUser = new User();
+        updateUser.setId(user.getId());
+        updateUser.setEmail(user.getEmail());
+        updateUser.setPhone(user.getPhone());
+        updateUser.setQuestion(user.getQuestion());
+        updateUser.setAnswer(user.getAnswer());
+        int updateCount = userMapper.updateByPrimaryKeySelective(updateUser);
+        if (updateCount > 0) {
+            return ServerResponse.creatBySuccess("更新成功", updateUser);
+        }
+        return ServerResponse.createByErrorMessage("更新失敗");
+
+
+    }
+
+    public ServerResponse<User> getInformation(Integer userId){
+        User user = userMapper.selectByPrimaryKey(userId);
+        if(user == null){
+            return  ServerResponse.createByErrorMessage("找不到當前用戶");
+        }
+        user.setPassword(StringUtils.EMPTY); //不把密碼返回到http內
+        return ServerResponse.creatBySuccess(user);
+    }
+}
